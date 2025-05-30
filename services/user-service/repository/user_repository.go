6979eb1d6ccbd108b2b75f061bf417a4677b0733
thehuy1805 <107227703+thehuy1805@users.noreply.github.com>
2025-05-30 14:57:44 +0700

    Crete User-service

---
+++ services/user-service/repository/user_repository.go
@@ -0,0 +1,51 @@
+package repository
+
+import (
+	"services/user-service/model"
+
+	"gorm.io/gorm"
+)
+
+type UserRepository interface {
+	Create(user *model.User) error
+	FindByEmail(email string) (*model.User, error)
+	FindByID(id string) (*model.User, error)
+	Update(user *model.User) error
+	Delete(id string) error
+}
+
+type userRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) UserRepository {
+	return &userRepository{db}
+}
+
+func (r *userRepository) Create(user *model.User) error {
+	return r.db.Create(user).Error
+}
+
+func (r *userRepository) FindByEmail(email string) (*model.User, error) {
+	var user model.User
+	if err := r.db.Where("email = ?", email).First(&user).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (r *userRepository) FindByID(id string) (*model.User, error) {
+	var user model.User
+	if err := r.db.Where("id = ?", id).First(&user).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (r *userRepository) Update(user *model.User) error {
+	return r.db.Save(user).Error
+}
+
+func (r *userRepository) Delete(id string) error {
+	return r.db.Delete(&model.User{}, "id = ?", id).Error
+}
